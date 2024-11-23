590de9568ab5005b0a45c76640ba63d9d917ccf9
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2024-11-23 14:01:53 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>TODO</h1>
+    <form action="" method="post">
+        <input type="text">
+    </form>
+    <ul>
+        <li>テストする</li>
+        <li>仕事する</li>
+        <li>家にかえる</li>
+    </ul>
+</body>
+
+</html>
